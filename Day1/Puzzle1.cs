a4f4455a0c7c9b4a21ba5a6aa6abaa4c2421f0af
Richard Routley <richardroutley@gmail.com>
2024-12-02 21:18:33 +0000

    Day 1

---
+++ Day1/Puzzle1.cs
@@ -0,0 +1,36 @@
+using static System.Math;
+class Puzzle1 : IPuzzle
+{
+    public void Excute()
+    {
+        var lines = sample.Split("\r\n");
+        lines = File.ReadAllLines("Day1\\input.txt");
+
+        var left = lines.Select(a => a.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0]).Select(int.Parse).ToList();
+        var right = lines.Select(a => a.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]).Select(int.Parse).ToList();
+
+        var result = left.OrderBy(a => a)
+                         .Zip(right.OrderBy(b => b))
+                         .Select(item => Abs(item.First - item.Second))
+                         .Sum(z => z);
+        System.Console.WriteLine("Part 1 = {0}",result);
+
+
+        var freq = right.GroupBy(a => a)
+                        .ToDictionary(k => k.Key, v => v.Count());
+        result = left.Join(freq, l => l, r => r.Key, (l, r) => l * r.Value).Sum();
+
+        System.Console.WriteLine("Part 2 = {0}",result);
+    }
+
+
+
+    private string sample = """
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+""";
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+var lastPuzzleType = Assembly.GetEntryAssembly()
+                             .GetTypes()
+                             .Where(t => typeof(IPuzzle).IsAssignableFrom(t) && t.IsClass)
+                             .OrderByDescending(t => int.Parse(t.Name[6..]))
+                             .First();
+
+var puzzle = (IPuzzle)Activator.CreateInstance(lastPuzzleType);
+
+
+System.Console.WriteLine("Running {0}", lastPuzzleType.Name);
+puzzle.Excute();
