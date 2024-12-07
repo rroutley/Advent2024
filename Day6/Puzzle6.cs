43859bddf69d1a14e09c414b63e11cdc18acd9eb
Richard Routley <richardroutley@gmail.com>
2024-12-07 11:05:19 +0000

    Day 6 Part 2

--- Day6/Puzzle6.cs
+++ Day6/Puzzle6.cs
@@ -1,5 +1,6 @@
-// #define Sample
+//#define Sample
 using Point2d = (int x, int y);
+using Point3d = (int x, int y, int z);
 using Vector2d = (int x, int y);
 public class Puzzle6 : IPuzzle
 {
@@ -32,13 +33,16 @@ public class Puzzle6 : IPuzzle
                 if (grid[y][x] == '^')
                     guard = (x, y);
 
-        int result = Part1(grid, guard);
+        var result = Part1(grid, guard);
 
-        System.Console.WriteLine("Part 1 = {0}", result);
+        System.Console.WriteLine("Part 1 = {0}", result.Count);
 
+        int count = Part2(grid, guard, result);
+
+        System.Console.WriteLine("Part 2 = {0}", count);
     }
 
-    private int Part1(string[] grid, Point2d guard)
+    private HashSet<Point2d> Part1(string[] grid, Point2d guard)
     {
         HashSet<Point2d> visited = new();
         int heading = 0;
@@ -64,10 +68,67 @@ public class Puzzle6 : IPuzzle
             }
         }
 
-        return visited.Count;
+        return visited;
     }
 
 
+    private int Part2(string[] grid, (int x, int y) guard, HashSet<(int x, int y)> result)
+    {
+        int count = 0;
+
+        foreach (var point in result)
+        {
+            if (point == guard) continue;
+
+            string original = grid[point.y];
+            grid[point.y] = $"{original[..point.x]}O{original[(point.x + 1)..]}";
+
+            if (IsLoop(grid, guard))
+            {
+                System.Console.WriteLine($"Loop Found {point}");
+                count++;
+            }
+            grid[point.y] = original;
+        }
+
+        return count;
+    }
+
+    private bool IsLoop(string[] grid, Point2d guard)
+    {
+        HashSet<Point3d> visited = new();
+        int heading = 0;
+        while (true)
+        {
+            var added = visited.Add((guard.x, guard.y, heading));
+
+            // Have we been here and going in this direction before?
+            if (!added)
+            {
+                return true;
+            }
+
+            // Next Forward step
+            Point2d next = (guard.x + directions[heading].x, guard.y + directions[heading].y);
+
+            // Are we off the grid?
+            if (next.x < 0 || next.x >= cols || next.y < 0 || next.y >= rows)
+            {
+                return false;
+            }
+
+            char v = grid[next.y][next.x];
+            if (v == '#' || v == 'O')
+            {
+                // Turn clockwise
+                heading = (heading + 1) % 4;
+            }
+            else
+            {
+                guard = next;
+            }
+        }
+    }
 
     private string sample = """
 ....#.....
