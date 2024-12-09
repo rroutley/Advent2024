a30cf28915405fe0e8f8674de4588837c56076ad
Richard Routley <richardroutley@gmail.com>
2024-12-09 22:49:09 +0000

    Day 9 Part 2

--- Day9/Puzzle9.cs
+++ Day9/Puzzle9.cs
@@ -16,7 +16,7 @@ public class Puzzle9 : IPuzzle
         var line = lines[0];
 
 
-        var fileSystem = LoadFiles(line);
+        var fileSystem = LoadFiles(line, [], []);
 
         Render(fileSystem);
 
@@ -24,18 +24,115 @@ public class Puzzle9 : IPuzzle
 
         Render(fileSystem);
 
-
-
-
-
         long result = Checksum(fileSystem);
 
         Console.WriteLine("Part 1 = {0}", result);
 
 
 
+        List<Block> files = new();
+        List<Block> freeList = new();
+        fileSystem = LoadFiles(line, files, freeList);
+
+        Render(fileSystem);
+
+        DefragementFiles(fileSystem, files, freeList);
+
+        Render(fileSystem);
+
+        result = Checksum(fileSystem);
+
         Console.WriteLine("Part 2 = {0}", result);
 
+    }
+
+    private void DefragementFiles(List<int> fileSystem, List<Block> files, List<Block> freeList)
+    {
+        for (int f = files.Count - 1; f >= 0; f--)
+        {
+            var file = files[f];
+            var freeBlock = FindFreeBlock(file);
+            if (freeBlock != null)
+            {
+                // Move the File
+                for (int i = 0; i < file.Length; i++)
+                {
+                    fileSystem[freeBlock.Start + i] = file.File;
+                    fileSystem[file.Start + i] = SPARE;
+                }
+                files[f] = new Block(freeBlock.Start, file.Length, file.File);
+
+                // Shrink the Free block for the destination
+                var remainingSpace = freeBlock.Length - file.Length;
+                var fi = freeList.IndexOf(freeBlock);
+
+                if (remainingSpace == 0)
+                {
+                    // entire block was used
+                    freeList.RemoveAt(fi);
+                }
+                else if (remainingSpace > 0)
+                {
+                    freeList[fi] = new Block(freeBlock.Start + file.Length, remainingSpace, SPARE);
+                }
+
+
+                // Add a Free Block where the file used to be
+                var start = file.Start;
+                var length = file.Length;
+                // Consider Free Blocks either side of where the file used to be and merge if possible
+                var adjLeft = freeList.FindIndex(b => b.Start + b.Length == file.Start);
+                var adjRight = freeList.FindIndex(b => b.Start == file.Start + file.Length);
+                if (adjLeft >= 0 && adjRight >= 0)
+                {
+                    // Enlarge the block to the left and remove the block on the right
+                    freeList[adjLeft] = new Block(freeList[adjLeft].Start, freeList[adjLeft].Length + length + freeList[adjRight].Length, SPARE);
+                    freeList.RemoveAt(adjRight);
+                }
+                else if (adjRight >= 0)
+                {
+                    // Enlarge the block to the right
+                    freeList[adjRight] = new Block(start, length + freeList[adjRight].Length, SPARE);
+                }
+                else if (adjLeft >= 0)
+                {
+                    // Enlarge the block to the left
+                    freeList[adjLeft] = new Block(freeList[adjLeft].Start, freeList[adjLeft].Length + length, SPARE);
+                }
+                else
+                {
+                    // Insert a new Free block where the file was.
+                    var i = freeList.FindIndex(b => b.Start > start + length);
+                    var item = new Block(start, length, SPARE);
+                    if (i >= 0)
+                        freeList.Insert(i, item);
+                    else
+                        freeList.Add(item);
+                }
+
+
+                Render(fileSystem);
+            }
+
+        }
+
+        Block FindFreeBlock(Block file)
+        {
+            for (int b = 0; b < freeList.Count; b++)
+            {
+                // Only interested in free blocks to the left of the file
+                if (freeList[b].Start > file.Start)
+                    break;
+
+                if (file.Length <= freeList[b].Length)
+                {
+                    var freeBlock = freeList[b];
+                    return freeBlock;
+
+                }
+            }
+            return null;
+        }
     }
 
     private long Checksum(List<int> fileSystem)
@@ -78,7 +175,7 @@ public class Puzzle9 : IPuzzle
         Console.WriteLine();
     }
 
-    private static List<int> LoadFiles(string line)
+    private static List<int> LoadFiles(string line, List<Block> files, List<Block> free)
     {
         Console.WriteLine(line);
 
@@ -86,17 +183,25 @@ public class Puzzle9 : IPuzzle
         int fileNum = 0;
 
         var nums = line.Select(c => c - '0').GetEnumerator();
+        int current;
 
         while (nums.MoveNext())
         {
             int fileSize = nums.Current;
 
+            current = fileSystem.Count;
             fileSystem.AddRange(Enumerable.Repeat(fileNum, fileSize));
+            files.Add(new Block(current, fileSize, fileNum));
 
             if (nums.MoveNext())
             {
                 var padding = nums.Current;
-                fileSystem.AddRange(Enumerable.Repeat(SPARE, padding));
+                if (padding > 0)
+                {
+                    current = fileSystem.Count;
+                    fileSystem.AddRange(Enumerable.Repeat(SPARE, padding));
+                    free.Add(new Block(current, padding, SPARE));
+                }
             }
 
             fileNum++;
@@ -104,6 +209,9 @@ public class Puzzle9 : IPuzzle
 
         return fileSystem;
     }
+
+
+    record Block(int Start, int Length, int File);
 
     private string sample = """
 2333133121414131402
